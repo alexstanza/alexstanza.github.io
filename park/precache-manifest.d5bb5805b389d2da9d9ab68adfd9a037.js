30fa32fcd569106e9cfb5ac107995a63293f46f2
alexstanza <alexander.stanza@gmail.com>
2022-07-01 15:37:59 +0800

    Initial commit nearest parking

---
+++ park/precache-manifest.d5bb5805b389d2da9d9ab68adfd9a037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ba4c54bc7ade2da829",
+    "url": "/park/static/css/main.c2a4be96.chunk.css"
+  },
+  {
+    "revision": "11ba4c54bc7ade2da829",
+    "url": "/park/static/js/main.e53a5aa0.chunk.js"
+  },
+  {
+    "revision": "7246ef0bf92b5bbc6f49",
+    "url": "/park/static/js/runtime~main.bcf2567d.js"
+  },
+  {
+    "revision": "5cfbce71973b236f9228",
+    "url": "/park/static/css/2.78185d43.chunk.css"
+  },
+  {
+    "revision": "5cfbce71973b236f9228",
+    "url": "/park/static/js/2.e5477914.chunk.js"
+  },
+  {
+    "revision": "2ed70a1a2df1f93a1f29",
+    "url": "/park/static/js/3.9209433f.chunk.js"
+  },
+  {
+    "revision": "5e0193bec8bd7f0138363ce0f36ef828",
+    "url": "/park/index.html"
+  }
+];
