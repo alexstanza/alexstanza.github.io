a4b46d86c2f772e32f5af257bb50bdbe4be07bb0
alexstanza <alexander.stanza@gmail.com>
2022-07-05 09:03:57 +0800

    Added HDB parking availability

--- park/precache-manifest.03deb5dab16191b4d45b0d8421644992.js
+++ park/precache-manifest.03deb5dab16191b4d45b0d8421644992.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "ebf797386c7f2fa34b03",
+    "revision": "90e44c2bde205640276a",
     "url": "/park/static/css/main.c2a4be96.chunk.css"
   },
   {
-    "revision": "ebf797386c7f2fa34b03",
-    "url": "/park/static/js/main.f12bce67.chunk.js"
+    "revision": "90e44c2bde205640276a",
+    "url": "/park/static/js/main.f90547f7.chunk.js"
   },
   {
     "revision": "7246ef0bf92b5bbc6f49",
     "url": "/park/static/js/runtime~main.bcf2567d.js"
   },
   {
-    "revision": "600571958783d27e8673",
+    "revision": "29ce546c8257594d0744",
     "url": "/park/static/css/2.78185d43.chunk.css"
   },
   {
-    "revision": "600571958783d27e8673",
-    "url": "/park/static/js/2.88063c29.chunk.js"
+    "revision": "29ce546c8257594d0744",
+    "url": "/park/static/js/2.f90322f1.chunk.js"
   },
   {
     "revision": "2ed70a1a2df1f93a1f29",
     "url": "/park/static/js/3.9209433f.chunk.js"
   },
   {
-    "revision": "bc5d43bf472ea232e82594a6aee01f78",
+    "revision": "c267479edec64ea01f69aff7c9a2aca2",
     "url": "/park/index.html"
   }
 ];
