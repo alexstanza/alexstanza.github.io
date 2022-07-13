054bb5b97693e2e8ddb7fed840262bf708fb6a7a
alexstanza <alexander.stanza@gmail.com>
2022-07-14 00:24:51 +0800

    Minor update on ver 0.2.0

---
+++ park/precache-manifest.497328b5fdde4f93e9ccbf2315e64c20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5ae69684e7344e7dd14",
+    "url": "/park/static/css/main.66ea43ae.chunk.css"
+  },
+  {
+    "revision": "b5ae69684e7344e7dd14",
+    "url": "/park/static/js/main.8ef66be8.chunk.js"
+  },
+  {
+    "revision": "7246ef0bf92b5bbc6f49",
+    "url": "/park/static/js/runtime~main.bcf2567d.js"
+  },
+  {
+    "revision": "f1627decd4e90f0af397",
+    "url": "/park/static/css/2.9b14ef3f.chunk.css"
+  },
+  {
+    "revision": "f1627decd4e90f0af397",
+    "url": "/park/static/js/2.aac50bce.chunk.js"
+  },
+  {
+    "revision": "2ed70a1a2df1f93a1f29",
+    "url": "/park/static/js/3.9209433f.chunk.js"
+  },
+  {
+    "revision": "5b58ee4638820bfcdad1a4c7bfecf74f",
+    "url": "/park/index.html"
+  }
+];
--- park/service-worker.js
+++ park/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/park/precache-manifest.89f12b542fc041e7823231b0bc1e6508.js"
+  "/park/precache-manifest.497328b5fdde4f93e9ccbf2315e64c20.js"
 );
 
 workbox.clientsClaim();
